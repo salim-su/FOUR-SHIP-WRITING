9892698228ede257065cfe70887e75898264815d
salim-su <salimsucc@gmail.com>
2023-12-27 16:11:11 +0800

    SALIM:四公司作业写实

--- src/views/ship/real-writing-submit.vue
+++ src/views/ship/real-writing-submit.vue
@@ -34,13 +34,10 @@
       <van-uploader
         ref='files'
         :disabled='disabled'
-        :file-list='fileList'
+        :file-list='photoCar'
         :max-count='3'
-        :upload-text="'上传'"
-        :preview-full-image='false'
-        :use-before-read='true'
         accept='image/*'
-        :after-read='afterRead'
+        :after-read='uploadFilePerson'
       >
         <div></div>
       </van-uploader>
@@ -101,7 +98,9 @@ export default {
       fileList: [],
       hatch: '',
       hatchSel: '',
-      disabled: false
+      disabled: false,
+      photoCar: [],
+      photoIDCar: []
     }
   },
   mounted() {
@@ -121,6 +120,57 @@ export default {
     ]
   },
   methods: {
+    getFileFromBase64(base64URL, filename) {
+      var arr = base64URL.split(',')
+      var mime = arr[0].match(/:(.*?);/)[1]
+      var bstr = atob(arr[1])
+      var n = bstr.length
+      var u8arr = new Uint8Array(n)
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+      }
+      return new File([u8arr], filename, { type: mime })
+    },
+    uploadFilePerson(file) {
+      file.status = 'uploading'
+      file.message = '上传中...'
+      if (/\/(?:jpeg|png)/i.test(file.file.type)) {
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+        const img = new Image()
+        img.src = file.content
+        img.onload = () => {
+          const square = 0.3
+          const imageWidth = Math.round(square * img.width)
+          const imageHeight = Math.round(square * img.height)
+          canvas.width = imageWidth
+          canvas.height = imageHeight
+          context.drawImage(img, 0, 0, imageWidth, imageHeight)
+          file.content = canvas.toDataURL(file.file.type, 0.6)
+          const name = file.file.name
+          file.file = this.getFileFromBase64(file.content, name)
+        }
+      }
+      setTimeout(() => {
+        const param = new FormData()
+        param.append('file', file.file)
+        param.append('code', 'smartgate')
+        this.$axios.post('/api/blade-resource/oss/endpoint/put-file-attach', param).then((res) => {
+          if (res.status === 200) {
+            file.status = 'done'
+            file.message = '上传成功'
+            this.photoIDCar.push(res.data.data.attachId)
+          } else {
+            file.status = 'failed'
+            file.message = '上传失败'
+          }
+          this.photoCar.push({
+            url: res.data.data.link
+            // url: 'https://img01.yzcdn.cn/vant/leaf.jpg'
+          })
+        })
+      }, 1000)
+    },
     listInfo() {
       const params = {
         shipId: '123123123',
@@ -132,19 +182,21 @@ export default {
       // ShipRealWritingList
     },
     subInfo() {
-      const data = {
-        shipId: '123123123',
-        cabin: '1',
-        log: '我是日志',
-        attachmentDetails: [
-          { ossId: '1', hatch: 'qian' },
-          { ossId: '2', hatch: 'hou' }
-        ]
-      }
-      console.log(data)
-      ShipRealWriting(data).then(res => {
-        console.log(res)
-      })
+      console.log(this.photoCar)
+      console.log(this.photoIDCar)
+      // const data = {
+      //   shipId: '123123123',
+      //   cabin: '1',
+      //   log: '我是日志',
+      //   attachmentDetails: [
+      //     { ossId: '1', hatch: 'qian' },
+      //     { ossId: '2', hatch: 'hou' }
+      //   ]
+      // }
+      // console.log(data)
+      // ShipRealWriting(data).then(res => {
+      //   console.log(res)
+      // })
     },
     tes() {
       alert(11)
